64c49ff6dda23088a0559c16e31e0f67b71ea51a
MarinaSergeyeva <morrrinad@gmail.com>
2020-08-12 17:50:20 +0300

    Updates

---
+++ precache-manifest.82daac9ac46e62c51e4ee2eacf603d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8466e8e4297e3b1b51e22f0739bd0b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "60b37b692a3aa6c307e7",
+    "url": "/goit-react-hw-01-components/static/css/main.49c96d1f.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b37b692a3aa6c307e7",
+    "url": "/goit-react-hw-01-components/static/js/main.6f71c598.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
